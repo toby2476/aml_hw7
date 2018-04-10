ec367a4d2ca9a6ce9a9cc5c13ba20a22922f8646
Abhinav Das <abhinav.das1997@gmail.com>
2018-04-09 19:35:16 -0500

    in progress

--- Code/q1.py
+++ Code/q1.py
@@ -24,18 +24,42 @@ def import_data():
 
 def initialize_em(num_topics,x):
 	
-	p = np.random.rand(len(x[:,0]),num_topics)
-	p = p/np.sum(p,axis=0)
-	pi = np.random.rand(num_topics)
-	pi = pi/np.sum(pi,axis=0)
+	p = np.random.rand(len(x[0,:]),num_topics)
+	p = p/np.transpose(np.repeat(np.expand_dims(np.sum(p,axis=0),axis=1),len(x[0,:]),axis=1))
+	pi = np.ones((num_topics))/num_topics
 	return p,pi
-	
+
+def e_step(x,p,pi):
+	print('start e step')
+	p[p<1e-300] = 1e-300
+	w = np.dot(x,np.log(p))+np.log(pi)
+	max_val = np.amax(w, axis=1)
+	w = np.exp(w - max_val[:,None])
+	w = w/((np.sum(w,axis=1))[:,None])
+	return w
+
+def m_step(x,w):
+	print('start m step')
+	p = np.matmul(np.transpose(x),w)
+	p = p/np.transpose(np.repeat(np.expand_dims(np.sum(p,axis=0),axis=1),len(x[0,:]),axis=1))
+
+	pi = np.sum(w,axis=0)/len(w)
+	return p, pi	
+
+def EM(num_topics,x):
+	p,pi = initialize_em(num_topics,x)
+	for i in range(10):
+		w = e_step(x,p,pi)
+		p,pi = m_step(x,w)
+	return [w,p,pi]
 
 def main():
 	
 	x = import_data()
-	p,pi = initialize_em(30,x)
+	w,p,pi = EM(30,x)
 	print(p)
 	print(pi)
+	
+	
 
 main()
